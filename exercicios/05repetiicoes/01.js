038c8c49754e5efb626aabcf34b43fac3a9858ff
vitorsoaresdev <dev.vitors@gmail.com>
2022-06-08 21:06:22 -0300

    att

---
+++ exercicios/05repetiicoes/01.js
@@ -0,0 +1,25 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
+
+
+/*var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+} */
+
+
+
+/*console.log('Passo1')
+console.log('Passo2')
+console.log('Passo3')
+console.log('Passo4')
+console.log('Passo5')
+console.log('Passo6')
+console.log('Passo7')
+console.log('Passo8')
+console.log('Passo9')
+console.log('Passo10') */
---
+++ exercicios/05repetiicoes/02.js
@@ -0,0 +1,5 @@
+var idade = 26
+
+while (idade > 18) {
+    console.log(`O senhor(a) tem ${idade} anos, Seja bem vindo(a)!`)
+}
